18d7078cf40046d11110ac3a7bfdb5ed4739eb2a
IAmPrem7685 <prempainkara1@gmail.com>
2021-11-27 15:58:53 +0000

    added navbar

--- beta/frontend/my-app/src/App.js
+++ beta/frontend/my-app/src/App.js
@@ -1,24 +1,26 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Navbar from "./componet/Navbar";
 import Home from "./componet/Home";
 import ProductList from "./componet/ProductList";
 import ProductPage from "./componet/ProductPage";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Profile from "./componet/Profile";
 import SellPage from "./componet/SellPage";
-import LoginPage from "./componet/LoginPage";
-gggg
+// import LoginPage from "./componet/LoginPage";
+
 function App() {
   return (
     <div className="App">
       <Router>
+      <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/ProductPage" element={<ProductPage />} />
           <Route path="/ProductList" element={<ProductList />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/SellPage" element={<SellPage />} />
-          <Route path="/LoginPage" element={<LoginPage />} />
+          {/* <Route path="/LoginPage" element={<LoginPage />} /> */}
         </Routes>
       </Router>
     </div>
