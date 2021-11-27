7f47d3582048b32249b2819b1016e941e960a899
prem <prempainkara1@gmail.com>
2021-11-27 19:53:52 +0530

    starting project

---
+++ beta/frontend/my-app/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./componet/Home";
+import ProductList from "./componet/ProductList";
+import ProductPage from "./componet/ProductPage";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Profile from "./componet/Profile";
+import SellPage from "./componet/SellPage";
+import LoginPage from "./componet/LoginPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/ProductPage" element={<ProductPage />} />
+          <Route path="/ProductList" element={<ProductList />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/SellPage" element={<SellPage />} />
+          <Route path="/LoginPage" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ beta/frontend/my-app/src/componet/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      home------------------fdlsjflsdlfdsf dfbsdbfljsdfsdlf fdfljsdflsf ----
+    </div>
+  );
+};
+
+export default Home;
